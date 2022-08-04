69602bfcd8c627e6456bbe0b7a0cdb11076ed836
Kevin <kevin.acuna@ivitec.com.mx>
2022-08-04 11:21:58 -0500

    v.0.5.0 Se genero CRUD para la seccion tipo alianza

--- views/tg_cte_tipo_alianza/alta.php
+++ views/tg_cte_tipo_alianza/alta.php
@@ -1,15 +1,18 @@
-<?php /** @var controllers\controlador_org_empresa $controlador */ ?>
+<?php /** @var \tglobally\tg_cliente\controllers\controlador_tg_cte_tipo_alianza $controlador */ ?>
 <?php include 'templates/tg_cte_tipo_alianza/alta/secciones.php'; ?>
 <div class="col-md-9 formulario">
     <div class="col-lg-12">
 
         <h3 class="text-center titulo-form">Hola, <?php echo $controlador->datos_session_usuario['adm_usuario_user']; ?> </h3>
 
-
         <div class="  form-main" id="form">
-            <form method="post" action="./index.php?seccion=org_empresa&accion=alta_bd&session_id=<?php echo $controlador->session_id; ?>" class="form-additional">
+            <form method="post" action="./index.php?seccion=tg_cte_tipo_alianza&accion=alta_bd&session_id=<?php echo $controlador->session_id; ?>" class="form-additional">
 
                 <?php echo $controlador->inputs->codigo; ?>
+                <?php echo $controlador->inputs->codigo_bis; ?>
+                <?php echo $controlador->inputs->descripcion; ?>
+                <?php echo $controlador->inputs->descripcion_select; ?>
+                <?php echo $controlador->inputs->alias; ?>
                 <div class="buttons col-md-12">
                     <div class="col-md-6">
                         <button type="submit" class="btn btn-info btn-guarda col-md-12 " name="btn_action_next" value="modifica">Guarda</button>
