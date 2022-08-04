3f39c2afae9bd90ef8a219ee273d89636a102b9b
Kevin <kevin.acuna@ivitec.com.mx>
2022-08-04 12:46:43 -0500

    v.0.8.0 Se genero action modifica en tipo alianza

--- templates/tg_cte_tipo_alianza/modifica/secciones.php
+++ templates/tg_cte_tipo_alianza/modifica/secciones.php
@@ -1,32 +1,15 @@
-<?php /** @var controllers\controlador_org_empresa $controlador */ ?>
+<?php /** @var \tglobally\tg_cliente\controllers\controlador_tg_cte_tipo_alianza $controlador */?>
+<?php use config\views; ?>
+
 <div class="col-md-3 secciones">
 
     <div class="col-md-12 int_secciones ">
-        <div class="col-md-12 ">
-            <h4 class="sub_text_datos">RFC:</h4>
-            <h5 class="text_datos"><?php echo $controlador->rfc;?></h5>
-            <h4 class="sub_text_datos">Razón social:</h4>
-            <h5 class="text_datos"><?php echo $controlador->razon_social;?></h5>
-        </div>
         <div class="col-md-4 seccion">
-            <img src="img/1.svg" class="img-seccion">
+            <img src="<?php echo (new views())->url_assets.'img/stepper/1.svg'?>" class="img-seccion">
         </div>
         <div class="col-md-8">
-            <h3>Empresas</h3>
+            <h3>Alta de clientes</h3>
             <?php include "templates/tg_cte_tipo_alianza/_base/buttons/1.azul.php"; ?>
-            <hr class="hr-menu-lateral">
-            <?php include "templates/tg_cte_tipo_alianza/_base/links/2.php"; ?>
-            <hr class="hr-menu-lateral">
-            <?php include "templates/tg_cte_tipo_alianza/_base/links/3.php"; ?>
-            <hr class="hr-menu-lateral">
-            <?php include "templates/tg_cte_tipo_alianza/_base/links/4.php"; ?>
-            <hr class="hr-menu-lateral">
-            <?php include "templates/tg_cte_tipo_alianza/_base/links/5.php"; ?>
-            <hr class="hr-menu-lateral">
-            <?php include "templates/tg_cte_tipo_alianza/_base/links/6.php"; ?>
-            <hr class="hr-menu-lateral">
-            <?php include "templates/tg_cte_tipo_alianza/_base/links/7.php"; ?>
         </div>
-
     </div>
 </div>
---
+++ views/tg_cte_tipo_alianza/modifica.php
@@ -0,0 +1,29 @@
+<?php /** @var \tglobally\tg_cliente\controllers\controlador_tg_cte_tipo_alianza $controlador */ ?>
+<?php include 'templates/tg_cte_tipo_alianza/modifica/secciones.php'; ?>
+<div class="col-md-9 formulario">
+    <div class="col-lg-12">
+
+        <h3 class="text-center titulo-form">Hola, <?php echo $controlador->datos_session_usuario['adm_usuario_user']; ?> </h3>
+
+        <div class="  form-main" id="form">
+            <form method="post" action="./index.php?seccion=tg_cte_tipo_alianza&accion=modifica_generales&session_id=<?php echo $controlador->session_id; ?>&registro_id=<?php echo $controlador->registro_id; ?>" class="form-additional">
+
+                <?php echo $controlador->inputs->codigo; ?>
+                <?php echo $controlador->inputs->codigo_bis; ?>
+                <?php echo $controlador->inputs->descripcion; ?>
+                <?php echo $controlador->inputs->descripcion_select; ?>
+                <?php echo $controlador->inputs->alias; ?>
+                <div class="buttons col-md-12">
+                    <div class="col-md-6 btn-ancho">
+                        <button type="submit" class="btn btn-info btn-guarda col-md-12 " >Guarda</button>
+                    </div>
+                    <div class="col-md-6 btn-ancho">
+                        <button type="submit" class="btn btn-info btn-guarda col-md-12 " >Siguiente</button>
+                    </div>
+
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
