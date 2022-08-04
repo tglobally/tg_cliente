4656ae5f4da78226eb36770f6c3138a3cb95dd1c
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-04 15:18:19 -0500

    v.0.11.0 Se ajusta catalogode tipos de alianza

--- controllers/controlador_adm_session.php
+++ controllers/controlador_adm_session.php
@@ -13,13 +13,14 @@ use gamboamartin\errores\errores;
 use JsonException;
 use stdClass;
 
+
 class controlador_adm_session extends \gamboamartin\controllers\controlador_adm_session {
     public bool $existe_msj = false;
     public string $include_menu = '';
     public string $mensaje_html = '';
 
-    public string $link_alta_org_empresa = '';
-    public string $link_lista_org_empresa = '';
+    public string $link_alta_tg_cte_tipo_alianza = '';
+    public string $link_lista_tg_cte_tipo_alianza = '';
 
     /**
      * Funcion de controlador donde se ejecutaran siempre que haya un acceso denegado
@@ -55,11 +56,11 @@ class controlador_adm_session extends \gamboamartin\controllers\controlador_adm_
             return $this->retorno_error(mensaje:  'Error al generar template',data: $template, header: $header, ws: $ws);
         }
 
-        $hd = "index.php?seccion=org_empresa&accion=alta&session_id=$this->session_id";
-        $this->link_alta_org_empresa = $hd;
+        $hd = "index.php?seccion=tg_cte_tipo_alianza&accion=alta&session_id=$this->session_id";
+        $this->link_alta_tg_cte_tipo_alianza = $hd;
 
-        $hd = "index.php?seccion=org_empresa&accion=lista&session_id=$this->session_id";
-        $this->link_lista_org_empresa = $hd;
+        $hd = "index.php?seccion=tg_cte_tipo_alianza&accion=lista&session_id=$this->session_id";
+        $this->link_lista_tg_cte_tipo_alianza = $hd;
 
         $this->include_menu = (new generales())->path_base;
         $this->include_menu .= 'templates/inicio.php';
--- views/adm_session/inicio.php
+++ views/adm_session/inicio.php
@@ -1,4 +1,4 @@
-<?php /** @var controllers\controlador_adm_session $controlador */
+<?php /** @var tglobally\tg_cliente\controllers\controlador_adm_session $controlador */
 
 use config\views;
 $url_assets = (new views())->url_assets;
@@ -19,12 +19,12 @@ $url_assets = (new views())->url_assets;
     <div class="row">
         <div class="col-md-12">
             <div class="col-sm-2">
-                <a href="<?php echo $controlador->link_lista_org_empresa; ?>">
+                <a href="<?php echo $controlador->link_alta_tg_cte_tipo_alianza; ?>">
                     <div class="cont_imagen_accion">
                         <img src="<?php echo $url_assets; ?>img/inicio/imagen_2.jpg">
                     </div>
                     <div class="cont_text_accion">
-                        <h4 class="text_seccion">Empresas</h4>
+                        <h4 class="text_seccion">Tipos de Alianza</h4>
                         <h4 class="text_accion">Catologo</h4>
                     </div>
                 </a>
