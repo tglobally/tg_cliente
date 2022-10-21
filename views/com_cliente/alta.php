2e4ef42ec10a3dce3bc514002f17e6f857d6573e
Ivan Avelar <ivan_avelar6@hotmail.com>
2022-10-21 13:41:35 -0500

    v0.28.2 Se ajustan campos en alta

--- views/com_cliente/alta.php
+++ views/com_cliente/alta.php
@@ -8,6 +8,8 @@
         <div class="  form-main" id="form">
             <form method="post" action="./index.php?seccion=com_cliente&accion=alta_bd&session_id=<?php echo $controlador->session_id; ?>" class="form-additional">
 
+                <?php echo $controlador->inputs->codigo; ?>
+                <?php echo $controlador->inputs->descripcion; ?>
                 <?php echo $controlador->inputs->codigo_bis; ?>
                 <?php echo $controlador->inputs->rfc; ?>
                 <?php echo $controlador->inputs->razon_social; ?>
