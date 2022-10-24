934d6c9bb73be5f46662d109647b9ba68d709e6a
Ivan Avelar <ivan_avelar6@hotmail.com>
2022-10-24 12:29:31 -0500

    v0.29.2 Se integra lista de sucursal

---
+++ templates/com_sucursal/_base/links/alta.php
@@ -0,0 +1,4 @@
+<?php /** @var \tglobally\tg_cliente\controllers\controlador_com_producto $controlador */ ?>
+<a href="index.php?seccion=com_sucursal&accion=alta&session_id=<?php echo $controlador->session_id; ?>">
+    <?php include "templates/com_sucursal/_base/buttons/1.azul.alta.php"; ?>
+</a>
