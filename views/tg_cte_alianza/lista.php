dfb33862a6e6d060d4c4b0517684202594b5daa4
[user] <angel.martinez@ivitec.com.mx>
2022-10-28 14:39:34 -0500

    v0.42.6 Se implementa lista nueva cte alianza

--- views/tg_cte_alianza/lista.php
+++ views/tg_cte_alianza/lista.php
@@ -12,63 +12,43 @@ $url_icons = (new views())->url_icons;
     <div class="col-lg-12 content">
         <h3 class="text-center titulo-form">Hola, <?php echo $controlador->datos_session_usuario['adm_usuario_user']; ?></h3>
 
-        <div class="col-md-12 mt-3 table-responsive-sm">
+        <div class="lista">
+            <div class="card">
 
-            <div class="filters">
-                <div class="filter col-md-4 acciones_filter">
-                    <a class="icon_xls_lista">
-                        <img src="<?php echo $url_icons; ?>icon_xls.png">
-                    </a>
-                    <a class="icon_atras_lista">
-                        <img src="<?php echo $url_icons; ?>icon_pag_atras.svg">
-                    </a>
-                    <p class="paginador">3 de 35</p>
-                    <a class="icon_adelante_lista">
-                        <img src="<?php echo $url_icons; ?>icon_pag_adelante.svg">
-                    </a>
-                </div>
-                <div class="search col-md-8 input_search">
-                    <input type="text form-control input">
-                    <img class="input_icon" src="<?php echo $url_icons; ?>icon_lupa.svg">
-                </div>
-            </div>
+                <div class="card-body">
+                    <div class="cont_tabla_sucursal  col-md-12">
 
-            <table class="table table-dark">
-                <thead>
-                <tr>
-                    <th scope="col">Acciones</th>
-                    <th scope="col">Id</th>
-                    <th scope="col">Codigo</th>
-                    <th scope="col">Cod Bis</th>
-                    <th scope="col">Descripcion</th>
-                    <th scope="col">Tipo Alianza</th>
-                </tr>
-                </thead>
+            <table class="table ">
+                            <thead>
+                            <tr>
+                                <th data-breakpoints="xs sm md" data-type="html" >Id</th>
+                                <th data-breakpoints="xs sm md" data-type="html" >Codigo</th>
+                                <th data-breakpoints="xs sm md" data-type="html" >Cod Bis</th>
+                                <th data-breakpoints="xs sm md" data-type="html" >Descripcion</th>
+                                <th data-breakpoints="xs sm md" data-type="html" >Tipo Alianza</th>
+                                <th data-breakpoints="xs md" class="control"  data-type="html" data-filterable="false">Modifica</th>
+                                <th data-breakpoints="xs md" class="control"  data-type="html" data-filterable="false">Elimina</th>
+                            </tr>
+                            </thead>
                 <tbody>
                 <?php foreach ($controlador->registros as $registro){
 
                     ?>
                     <tr>
-                        <th class="colum_accion" scope="row">
-                            <a class="icon_modifica_lista" href="<?php echo $registro->link_modifica; ?>">
-                                <img src="<?php echo $url_icons; ?>icon_modifica.svg">
-                            </a>
-                            <a class="icon_elimina_lista" href="<?php echo $registro->link_elimina_bd; ?>">
-                                <img src="<?php echo $url_icons; ?>icon_elimina.svg">
-                            </a>
-
-                        </th>
-                        <th><?php echo $registro->tg_cte_alianza_id; ?></th>
-                        <th><?php echo $registro->tg_cte_alianza_codigo; ?></th>
-                        <th><?php echo $registro->tg_cte_alianza_codigo_bis; ?></th>
-                        <th><?php echo $registro->tg_cte_alianza_descripcion; ?></th>
-                        <th><?php echo $registro->tg_cte_tipo_alianza_descripcion; ?></th>
+                        <td><?php echo $registro->tg_cte_alianza_id; ?></td>
+                        <td><?php echo $registro->tg_cte_alianza_codigo; ?></td>
+                        <td><?php echo $registro->tg_cte_alianza_codigo_bis; ?></td>
+                        <td><?php echo $registro->tg_cte_alianza_descripcion; ?></td>
+                        <td><?php echo $registro->tg_cte_tipo_alianza_descripcion; ?></td>
+                        <td><a class="btn btn-warning " href="<?php echo $registro->link_modifica; ?>">Modifica</a></td>
+                        <td><a class="btn btn-danger " href="<?php echo $registro->link_elimina_bd; ?>">Elimina</a></td>
                     </tr>
                 <?php } ?>
-
                 </tbody>
             </table>
+                    </div>
+                </div>
+            </div>
         </div>
-
     </div>
 </div>
