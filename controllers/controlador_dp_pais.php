02b6e51229fcfe4656595368cbba732490c70594
[user] <angel.martinez@ivitec.com.mx>
2023-02-28 18:17:33 -0600

    v0.61.8 Se ajusta com sucursal

---
+++ controllers/controlador_dp_pais.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Martin Gamboa Vazquez
+ * @version 1.0.0
+ * @created 2022-05-14
+ * @final En proceso
+ *
+ */
+namespace tglobally\tg_cliente\controllers;
+
+
+class controlador_dp_pais extends \controllers\controlador_dp_pais {}
--- views/com_sucursal/modifica.php
+++ views/com_sucursal/modifica.php
@@ -8,17 +8,19 @@
         <div class="  form-main" id="form">
             <form method="post" action="./index.php?seccion=com_sucursal&accion=modifica_bd&session_id=<?php echo $controlador->session_id; ?>&registro_id=<?php echo $controlador->registro_id; ?>" class="form-additional">
 
+                <?php echo $controlador->inputs->com_tipo_sucursal_id; ?>
                 <?php echo $controlador->inputs->codigo; ?>
-                <?php echo $controlador->inputs->descripcion; ?>
                 <?php echo $controlador->inputs->nombre_contacto; ?>
-                <?php echo $controlador->inputs->select->dp_pais_id; ?>
-                <?php echo $controlador->inputs->select->dp_estado_id; ?>
-                <?php echo $controlador->inputs->select->dp_municipio_id; ?>
-                <?php echo $controlador->inputs->select->dp_cp_id; ?>
-                <?php echo $controlador->inputs->select->dp_colonia_id; ?>
-                <?php echo $controlador->inputs->select->dp_calle_pertenece_id; ?>
-                <?php echo $controlador->inputs->numero_interior; ?>
+                <?php echo $controlador->inputs->com_cliente_id; ?>
+                <?php echo $controlador->inputs->dp_pais_id; ?>
+                <?php echo $controlador->inputs->dp_estado_id; ?>
+                <?php echo $controlador->inputs->dp_municipio_id; ?>
+                <?php echo $controlador->inputs->dp_cp_id; ?>
+                <?php echo $controlador->inputs->dp_colonia_postal_id; ?>
+                <?php echo $controlador->inputs->dp_calle_pertenece_id; ?>
                 <?php echo $controlador->inputs->numero_exterior; ?>
+                <?php echo $controlador->inputs->numero_interior; ?>
+
                 <?php echo $controlador->inputs->telefono_1; ?>
                 <?php echo $controlador->inputs->telefono_2; ?>
                 <?php echo $controlador->inputs->telefono_3; ?>
